fc637eade06165c2b4a0caf61083fb1e741150e3
cipto <binarcipta02@gmail.com>
2024-06-27 13:49:26 +0700

    fix list dokter

--- modules/jaspel/views/tagihan/jaspel_temp.php
+++ modules/jaspel/views/tagihan/jaspel_temp.php
@@ -164,7 +164,7 @@ $this->params['breadcrumbs'][] = "Periode Jaspel : ".Jaspel::getBulan(Yii::$app-
                         $listDokterO = Yii::$app->db_jaspel
                             ->createCommand("SELECT a.`MEDIS`,c.`NAMA`
                             FROM layanan.`petugas_tindakan_medis` a
-                            LEFT JOIN master.`dokter` b ON b.`ID` = a.`MEDIS`
+                            LEFT JOIN master.`dokter` b ON b.`ID` = a.`MEDIS` AND b.`STATUS` = 1
                             LEFT JOIN master.`pegawai` c ON c.`NIP` = b.`NIP`
                             WHERE a.`TINDAKAN_MEDIS` = '".$item['idTindakanMedis']."' AND a.`STATUS` = 1 AND a.`JENIS` = 1 and a.`MEDIS` != 0
                             GROUP BY a.`MEDIS`")
